c8276925a08c0ec22708d455ac9bb4df5e976507
sovsh <soshatokh@yandex.ru>
2021-10-29 19:53:15 +0300

    Add export to json methods

--- lib/ruber_dialog/dialog.rb
+++ lib/ruber_dialog/dialog.rb
@@ -1,8 +1,24 @@
 module RuberDialog
   #
   class Dialog
+    attr_accessor :starting_node, :nodes, :characters
+
     def initialize(starting_node, nodes, characters)
       @starting_node, @nodes, @characters = starting_node, nodes, characters
+    end
+
+    def as_json(options = {})
+      {
+        starting_node: starting_node.to_json,
+        nodes: nodes.each do |node|
+          node.to_json
+        end,
+        characters: characters
+      }
+    end
+
+    def to_json(*options)
+      as_json(*options).to_json(*options)
     end
   end
 end
--- lib/ruber_dialog/parser/tokens.rb
+++ lib/ruber_dialog/parser/tokens.rb
@@ -21,6 +21,32 @@ module RuberDialog
           @name == other.name
         end
       end
+
+      class Line
+        attr_accessor :char_name, :phrase
+
+        def as_json(options = {})
+          {
+            char_name: char_name,
+            phrase: phrase
+          }
+        end
+
+        def to_json(*options)
+          as_json(*options).to_json(*options)
+        end
+      end
+
+      class Response
+        attr_accessor :response, :next_node
+
+        def as_json(options = {})
+          {
+            response: response,
+            next_node: next_node
+          }
+        end
+      end
     end
   end
 end
