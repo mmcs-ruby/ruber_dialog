ac44693a1ea44f0143d7830824ac3d8d2b8e4cbd
alexey-protopopov <58300541+alexey-protopopov@users.noreply.github.com>
2021-11-16 00:07:24 +0300

    Update ruber_dialog.rb

--- lib/ruber_dialog.rb
+++ lib/ruber_dialog.rb
@@ -5,6 +5,8 @@ require_relative "ruber_dialog/parser/errors"
 require_relative "ruber_dialog/parser/tokens"
 require_relative "ruber_dialog/parser/character_block_parser"
 require_relative "ruber_dialog/parser/block_parser"
+require_relative "ruber_dialog/parser/description_block_parser"
+require_relative "ruber_dialog/parser/description_parser"
 
 module RuberDialog
   class RuberArgumentError < ArgumentError; end
